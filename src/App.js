4f02c77ac3d5b991f633a6870ed26bada316c7c6
Ishwariiiii <ishwarikanathe@gmail.com>
2024-05-23 19:37:04 +0530

    add 1st commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App() {
+  const [todo, setToDo] = useState([])
+  const [input, setInput] = useState({
+    userid: "",
+    id: "",
+    title: "",
+    body: ""
+  })
 
-export default App;
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(response => response.json())
+      .then(resData => setToDo(resData))
+  }, [])
+
+  const handleInput = (e) => {
+    const name = e.target.name
+    const value = e.target.value
+    setInput({ ...input, [name]: value })
+  }
+  const saveData = (e) => {
+    e.preventDefault()
+
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(input)
+    }).then(response => response.json()).then(resData => {
+      setToDo([...todo, resData])
+      setInput({
+        userid: "",
+        id: "",
+        title: "",
+        body: ""
+      })
+    })
+  }
+
+  const deleteData=(index)=>{
+    fetch("https://jsonplaceholder.typicode.com/posts",{
+      method:"DELETE"
+    }).then(setToDo(todo.filter((ob,id)=>id!==index)))
+  }
+  return <div>
+
+    <form onSubmit={saveData}>
+      <input type="number" name="userid" value={input.userid} onChange={handleInput} placeholder="userId" />
+      <input type="number" name="id" value={input.id} onChange={handleInput} placeholder="Id" />
+      <input type="text" name="title" value={input.title} onChange={handleInput} placeholder="Title" />
+      <input type="text" name="body" value={input.body} onChange={handleInput} placeholder="Body" />
+      <button>Add</button>
+    </form>
+
+    <table className="table">
+      <thead>
+        <tr>
+          <th>S No.</th>
+          <th>UserId</th>
+          <th>Id</th>
+          <th>Title</th>
+          <th>Body</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        {todo.map((ob, index) => <tr key={index}>
+          <td>{index + 1}</td>
+          <td>{ob.userId}</td>
+
+          <td>{ob.id}</td>
+
+          <td>{ob.title}</td>
+          <td>{ob.body}</td>
+          <td><button Click={() => editData(index)}>Edit</button></td>
+          <td><button onClick={() => deleteData(index)}>Delete</button></td>
+        </tr>)}
+      </tbody>
+    </table>
+
+  </div>
+}``
